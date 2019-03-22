ccdc71229f5503e5cba9d847b92d609234a4827c
dexian <dxtang@yokozunadata.com>
2019-03-22 18:37:19 +0900

    how to use the image to fill the window?

--- src/routes/get.rs
+++ src/routes/get.rs
@@ -1,7 +1,15 @@
 use std::io;
+use std::option::Option;
+use rocket::http::RawStr;
 use rocket::response::{NamedFile};
+use std::{thread, time};
 
 #[get("/")]
 pub fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
+}
+
+#[get("/mandel?<w>&<h>")]
+pub fn test_ajax(w: Option<&RawStr>, h: Option<&RawStr>) -> String {
+    format!("your window: {}x{}", w.unwrap(), h.unwrap())
 }
--- static/main.js
+++ static/main.js
@@ -1,4 +1,18 @@
-$(window).resize(function() {
-    console.log(window.innerWidth, window.innerHeight)
-})
-console.log(window.innerWidth, window.innerHeight)
+const top_upper_left_x = -2;
+const top_upper_left_y = 1.25;
+
+const top_lower_right_x = 1;
+const top_lower_right_y = -1.25;
+
+
+function init() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+
+    $.get("/mandel", {"w": width, "h": height} ,function(data) {
+    console.log("succeed");
+    console.log(data)
+    })
+}
+
+init()
